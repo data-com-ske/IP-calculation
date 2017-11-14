1e15ba16d60cbed95cb02755721b352ae1c3f47c
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2017-11-14 23:44:30 +0700

    :bug: implement support exception throwed

--- src/main/java/edu/ske13/utils/IPUtils.java
+++ src/main/java/edu/ske13/utils/IPUtils.java
@@ -1,6 +1,7 @@
 package edu.ske13.utils;
 
 import edu.ske13.constants.Base;
+import edu.ske13.exception.IPException;
 import edu.ske13.exception.NotImplementedException;
 import edu.ske13.objects.IPAddress;
 import edu.ske13.objects.NumberBase;
@@ -11,16 +12,78 @@ import edu.ske13.objects.NumberBase;
  * @since Fri 10/Nov/2017 - 19:54
  */
 public class IPUtils {
-    public static IPAddress getIPAddress(String ipAddress) {
-        return new IPAddress(ipAddress);
+    private static IPAddress address;
+    
+    static {
+        try {
+            address = new IPAddress("1.0.0.0");
+        } catch (IPException e) {
+            // e.printStackTrace();
+        }
     }
     
-    public static IPAddress getIPAddress(int first, int second, int third, int fourth) {
-        return new IPAddress(first, second, third, fourth);
+    public static IPAddress forceCreateIPAddress(String ipAddress) {
+        try {
+            return getIPAddress(ipAddress);
+        } catch (IPException e) {
+            return null;
+        }
     }
     
-    public static IPAddress getIPAddress(String arg, String arg1, String arg2, String arg3) {
-        return new IPAddress(arg, arg1, arg2, arg3);
+    public static IPAddress forceCreateIPAddress(int first, int second, int third, int fourth) {
+        try {
+            return getIPAddress(first, second, third, fourth);
+        } catch (IPException e) {
+            return null;
+        }
+    }
+    
+    public static IPAddress forceCreateIPAddress(String arg, String arg1, String arg2, String arg3) {
+        try {
+            return getIPAddress(arg, arg1, arg2, arg3);
+        } catch (IPException e) {
+            return null;
+        }
+    }
+    
+    public static IPAddress getIPAddress(String ipAddress) throws IPException {
+        return address.updateIP(ipAddress);
+    }
+    
+    public static IPAddress getIPAddress(int first, int second, int third, int fourth) throws IPException {
+        return address.updateIP(first, second, third, fourth);
+    }
+    
+    public static IPAddress getIPAddress(String arg, String arg1, String arg2, String arg3) throws IPException {
+        return address.updateIP(arg, arg1, arg2, arg3);
+    }
+    
+    public static boolean checkIPAddress(String ipAddress) {
+        try {
+            return getIPAddress(ipAddress).isCorrect();
+        } catch (IPException e) {
+            return false;
+        }
+    }
+    
+    public static boolean checkIPAddress(int first, int second, int third, int fourth) {
+        try {
+            return getIPAddress(first, second, third, fourth).isCorrect();
+        } catch (IPException e) {
+            return false;
+        }
+    }
+    
+    public static boolean checkIPAddress(String arg, String arg1, String arg2, String arg3) {
+        try {
+            return getIPAddress(arg, arg1, arg2, arg3).isCorrect();
+        } catch (IPException e) {
+            return false;
+        }
+    }
+    
+    public static boolean checkIPAddress(IPAddress address) {
+        return address.isCorrect();
     }
     
     public static NumberBase getIPBase(IPAddress address, Base b) {
---
+++ src/test/java/edu/ske13/models/ipaddress/UpdatingTest.java
@@ -0,0 +1,9 @@
+package edu.ske13.models.ipaddress;
+
+/**
+ * @author kamontat
+ * @version 1.0
+ * @since Tue 14/Nov/2017 - 22:58
+ */
+public class UpdatingTest {
+}
