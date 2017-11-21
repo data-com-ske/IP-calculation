6257a2f18fc74f673c0d13ebfd65689ee8c7a2a8
emmytyro9 <witch_eam@hotmail.com>
2017-11-21 14:01:51 +0700

    update ui

--- src/main/java/edu/ske13/ui/main/MainView.java
+++ src/main/java/edu/ske13/ui/main/MainView.java
@@ -6,6 +6,8 @@ import edu.ske13.exception.NotImplementedException;
 import edu.ske13.utils.ui.Display;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 
 import static javax.swing.SwingUtilities.invokeLater;
@@ -13,27 +15,26 @@ import static javax.swing.SwingUtilities.invokeLater;
 public class MainView extends JFrame {
     private static MainView view = null;
     
-    // private JTextField ip1;
-    // private JTextField ip2;
-    // private JLabel dot1;
-    // private JTextField ip3;
-    // private JLabel dot2;
-    // private JTextField ip4;
-    // private JLabel SH_text;
-    // private JTextField sh;
-    // private JTable table;
-    // private JTable table_1;
-    // private JTable table_2;
+	private JTextField ip1;
+	private JTextField ip2;
+	private JLabel dot1;
+	private JTextField ip3;
+	private JLabel dot2;
+	private JTextField ip4;
+	private JLabel SH_text;
+	private JTextField sh;
+	private JTable table;
+	
+	private String ip;
+	private int sh_number;
     
     
-    private MainView() {
+    public MainView() {
         initialize();
     }
     
     private void compile(Point point) {
-        // FIXME: layout should not fix size [1]
-        // pack();
-        setMinimumSize(new Dimension(getWidth(), getHeight()));
+//        setMinimumSize(new Dimension(getWidth(), getHeight()));
         if (point != null) setLocation(point);
         else setLocation(Display.getDefaultPoint);
         setVisible(true);
@@ -83,159 +84,148 @@ public class MainView extends JFrame {
     /**************** controller usage method ****************/
     
     String getIP() {
-        throw new NotImplementedException();
+    	String value = ip1.getText();
+    	String value2 = ip2.getText();
+    	String value3 = ip3.getText();
+    	String value4 = ip4.getText();
+    	this.ip = String.format("%s.%s.%s.%s", value,value2,value3,value4);
+    	return ip;
     }
     
-    Integer getHostNumber() {
-        throw new NotImplementedException();
-    }
-    
-    Integer getSubnetNumber() {
-        throw new NotImplementedException();
-    }
-    
-    OutputType getChooseType() {
-        throw new NotImplementedException();
+    Integer getHostOrSubnetNumber() {
+    	String value = sh.getText();
+    	this.sh_number = Integer.parseInt(value);
+    	return sh_number;
     }
     
     void setIP(String ip) {
-        throw new NotImplementedException();
+        this.ip = ip;
     }
     
-    void setHostNumber(Integer number) {
-        throw new NotImplementedException();
+    void setHostOrSubnetNumber(Integer number) {
+        this.sh_number = number;
     }
     
-    void setSubnetNumber(Integer number) {
-        throw new NotImplementedException();
-    }
-    
-    void setChooseType(OutputType type) {
-        throw new NotImplementedException();
-    }
-    
-    void appendTable(/* TODO: input something */) {
-        throw new NotImplementedException();
+    void appendTable(String[][] data) {
+    	// row number 0 contains texts(which are header of each column)
+    	for (int i = 1; i < table.getRowCount(); i++) {
+    	      for(int j = 0; j < table.getColumnCount(); j++) {
+    	          table.setValueAt(data[i-1][j], i, j);
+    	      }
+    	 }
     }
     
     void clearTable() {
-        throw new NotImplementedException();
+    	for (int i = 1; i < table.getRowCount(); i++) {
+    	      for(int j = 0; j < table.getColumnCount(); j++) {
+    	          table.setValueAt("", i, j);
+    	      }
+    	 }
     }
     
     /**************** ui helper / validation ****************/
     
-    private boolean checkIP() {
-        throw new NotImplementedException();
-    }
-    
-    private void nextIPInput() {
-        throw new NotImplementedException();
-    }
-    
-    /**************** setting ui/ux ****************/
-    
-    private void settingTableComponent() {
-        throw new NotImplementedException();
-    }
-    
-    private void settingIPComponent() {
-        throw new NotImplementedException();
-    }
-    
-    private void settingSubnetComponent() {
-        throw new NotImplementedException();
-    }
-    
-    private void settingHostComponent() {
-        throw new NotImplementedException();
-    }
-    
-    private void settingLayout() {
-        throw new NotImplementedException();
-    }
-    
-    private void settingListeningEvent() {
-        throw new NotImplementedException();
-    }
+//    private boolean checkIP() {
+//        throw new NotImplementedException();
+//    }
+//    
+//    private void nextIPInput() {
+//        throw new NotImplementedException();
+//    }
     
     /**
      * Initialize the contents of the
      */
     private void initialize() {
-        
-        settingLayout();
-        settingIPComponent();
-        settingSubnetComponent();
-        settingHostComponent();
-        settingTableComponent();
-        
-        settingListeningEvent();
-        
-        // setLayout(null);
-        // FIXME: layout should not fix size [2]
-        // setSize(500, 300);
-        
-        // JLabel lblIp = new JLabel("IP: ");
-        // lblIp.setBounds(35, 28, 46, 14);
-        // getContentPane().add(lblIp);
-        
-        // ip1 = new JTextField();
-        // ip1.setBounds(91, 25, 53, 20);
-        // getContentPane().add(ip1);
-        // ip1.setColumns(10);
-        
-        // JLabel label = new JLabel(".");
-        // label.setBounds(154, 28, 11, 14);
-        // getContentPane().add(label);
-        
-        // ip2 = new JTextField();
-        // ip2.setColumns(10);
-        // ip2.setBounds(164, 25, 53, 20);
-        // getContentPane().add(ip2);
-        
-        // dot1 = new JLabel(".");
-        // dot1.setBounds(224, 28, 11, 14);
-        // getContentPane().add(dot1);
-        
-        // ip3 = new JTextField();
-        // ip3.setColumns(10);
-        // ip3.setBounds(237, 25, 53, 20);
-        // getContentPane().add(ip3);
-        
-        // dot2 = new JLabel(".");
-        // dot2.setBounds(300, 28, 11, 14);
-        // getContentPane().add(dot2);
-        
-        // ip4 = new JTextField();
-        // ip4.setColumns(10);
-        // ip4.setBounds(310, 25, 53, 20);
-        // getContentPane().add(ip4);
-        
-        // SH_text = new JLabel("Subnet/Host: ");
-        // SH_text.setBounds(10, 76, 81, 14);
-        // getContentPane().add(SH_text);
-        
-        // sh = new JTextField();
-        // sh.setBounds(92, 73, 271, 20);
-        // getContentPane().add(sh);
-        // sh.setColumns(10);
-        
-        // JScrollPane scrollPane = new JScrollPane();
-        // scrollPane.setBounds(35, 101, 427, 149);
-        // getContentPane().add(scrollPane);
-        
-        // table_2 = new JTable();
-        // table_2.setModel(new DefaultTableModel(new Object[][]{
-        //         {null, null, null, null, null},
-        //         {null, null, null, null, null},
-        //         {null, null, null, null, null},
-        //         {null, null, null, null, null},
-        //         {null, null, null, null, null},
-        //         {null, null, null, null, null},
-        //         {null, null, null, null, null},
-        // }, new String[]{
-        //         "Subnet", "Subnet ID", "1st Add", "Last Add", "Broadcast"
-        // }));
-        // scrollPane.setViewportView(table_2);
+ 
+ 		this.setBounds(200, 200, 550, 550);
+ 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 		this.setMinimumSize(new Dimension(400, 350));
+ 		this.getContentPane().setLayout(new GridLayout());
+		
+		Container containerMain = new Container() ;
+		BoxLayout layout = new BoxLayout(containerMain , BoxLayout.Y_AXIS) ;
+		containerMain.setLayout(layout) ;
+		this.getContentPane().add(containerMain);
+		
+		Container containerA = new Container() ;
+		containerMain.add(containerA) ;
+		FlowLayout layout1 = new FlowLayout() ;
+		containerA.setLayout(layout1) ;
+
+		Container containerB = new Container() ;
+		containerMain.add(containerB) ;
+		FlowLayout layout2 = new FlowLayout() ;
+		containerB.setLayout(layout2) ;
+		
+		Container containerC = new Container() ;
+		containerMain.add(containerC) ;
+		FlowLayout layout3 = new FlowLayout() ;
+		containerC.setLayout(layout3) ;
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Subnet", "Subnet ID", "1st Add", "Last Add", "Broadcast"},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+			},
+			new String[] {
+				"Subnet", "Subnet ID", "1st Add", "Last Add", "Broadcast"
+			}
+		));
+		containerC.add(table);
+		
+		JLabel lblIp = new JLabel("IP: ");
+		lblIp.setBounds(35, 28, 46, 14);
+		containerA.add(lblIp);
+		
+		ip1 = new JTextField();
+		ip1.setBounds(91, 25, 53, 20);
+		containerA.add(ip1);
+		ip1.setColumns(10);
+		
+		JLabel label = new JLabel(".");
+		label.setBounds(154, 28, 11, 14);
+		containerA.add(label);
+		
+		ip2 = new JTextField();
+		ip2.setColumns(10);
+		ip2.setBounds(164, 25, 53, 20);
+		containerA.add(ip2);
+		
+		dot1 = new JLabel(".");
+		dot1.setBounds(224, 28, 11, 14);
+		containerA.add(dot1);
+		
+		ip3 = new JTextField();
+		ip3.setColumns(10);
+		ip3.setBounds(237, 25, 53, 20);
+		containerA.add(ip3);
+		
+		dot2 = new JLabel(".");
+		dot2.setBounds(300, 28, 11, 14);
+		containerA.add(dot2);
+		
+		ip4 = new JTextField();
+		ip4.setColumns(10);
+		ip4.setBounds(310, 25, 53, 20);
+		containerA.add(ip4);
+		
+		
+		SH_text = new JLabel("Subnet/Host: ");
+		SH_text.setBounds(10, 76, 81, 14);
+		containerB.add(SH_text);
+		
+		sh = new JTextField();
+		sh.setBounds(92, 73, 271, 20);
+		containerB.add(sh);
+		sh.setColumns(10);
     }
 }
